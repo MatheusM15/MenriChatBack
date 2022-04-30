bf5823f94276d489b9a779199b32794b9adc6804
Matheus Ribeiro <matheusmenezesribeiro@hotmail.com>
2022-04-30 19:42:33 -0300

    Ajuste back end signal ir

--- WebApplication1/services/ChatHub.cs
+++ WebApplication1/services/ChatHub.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Cors;
+﻿using InfraMenriChat.Entity;
+using InfraMenriChat.Interface;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Threading.Tasks;
@@ -8,17 +10,38 @@ namespace WebApplication1.services
     public class ChatHub : Hub
     {
         private int userOnline = 0;
+        private readonly IMessageChatRepository _messageChatRepository;
+
+        public ChatHub(IMessageChatRepository messageChatRepository)
+        {
+            _messageChatRepository = messageChatRepository;
+        }
+
         public async Task senMensagem(string teste)
         {
             try
             {
-                if(!string.IsNullOrEmpty(teste))
-                    await Clients.All.SendAsync("Menssagem",teste);
+                var menssagem = new MessageChat
+                {
+                    DtAlteracao = DateTime.Now,
+                    DtInclusao = DateTime.Now,
+                    Message = teste
+                };
+                if(_messageChatRepository.InsertOrReplace(menssagem))
+                    await Clients.All.SendAsync("Menssagem", menssagem);
 
-            }catch(Exception e)
+
+
+            }
+            catch (Exception e)
             {
                 throw new Exception(e.Message);
             }
+        }
+        public async Task EscrevendoWith(bool teste)
+        {
+            if(teste)
+                await Clients.All.SendAsync("escrevendo", teste);
         }
     }
 }
